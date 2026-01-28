6de7a8b45f516e13f953d58da2abd6d15743c45a
Luksh Kumar <luksh.virjiani456@gmail.com>
2026-01-28 12:48:57 +0000

    variables and datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) WE ARE USING NODE JS, NOT BROWSER
+
+console.log(3+3)
+let name= "luksh" //string
+let age = 21 // int
+let isempty= true //boolean
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>stanalone value
+//undefined=>
+// symbol => unique
+
+//object
+
+console.log(typeof name) //string
+console.log(typeof null) //object
+console.log(typeof undefined) //itself datatype undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountid= 144553
+let accountEmail= "Luksh.virjiani456@gmail.com"
+var accountPassword= "Ou3348"
+accountCity= "karachi"
+let accountState; //undefined
+
+//accountid= 4 // not allowed to change with const
+
+accountEmail="dualcode@gmail.com"
+accountPassword= "78900"
+accountCity="hyderabad"
+
+console.log(accountid);
+
+/*
+Prefer not to use var
+because of issue in block scrope and functional scope
+*/
+
+console.table([accountid, accountEmail, accountPassword, accountCity, accountState]) 
